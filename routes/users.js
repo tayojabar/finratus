4386a4033fc0695b1041ece2fdcc5938e8296c2f
Tayo Jabar <tayojabar@gmail.com>
2018-09-04 15:33:43 +0100

    owners listing

---
+++ routes/users.js
@@ -0,0 +1,159 @@
+var express = require('express');
+var users = express.Router();
+//var database = require('../Database/database');
+var cors = require('cors')
+var jwt = require('jsonwebtoken');
+var token;
+users.use(cors());
+
+process.env.SECRET_KEY = "devesh";
+
+/* User Authentication */
+users.post('/login', function(req, res) {
+    var appData = {};
+    var username = req.body.email;
+    var password = req.body.password;
+    connection.query('SELECT * FROM users WHERE username = ?', [username], function(err, rows, fields) {
+            if (err) {
+                appData.error = 1;
+                appData["data"] = "Error Occured!";
+                //res.status(400).json(appData);
+                res.send(JSON.stringify(appData));
+            } else {
+                if (rows.length > 0) {
+                    if (rows[0].password == password) {
+                        let token = jwt.sign(rows[0], process.env.SECRET_KEY, {
+                            expiresIn: 1440
+                        });
+                        appData.error = 0;
+                        appData["token"] = token;
+                        appData["role"] = rows[0].user_role;
+                        // res.status(200).json(appData);
+                        res.send(JSON.stringify(appData));
+                    } else {
+                        appData.error = 1;
+                        appData["data"] = "Username and Password do not match";
+                        // res.status(204).json(appData);
+                        res.send(JSON.stringify(appData)); 
+                    }
+                } else {
+                    appData.error = 1;
+                    appData["data"] = "Email does not exists!";
+                    //res.status(204).json(appData);
+                    res.send(JSON.stringify(appData)); 
+                }
+            }
+        });
+   // connection.release();
+});
+
+/* Add New User */
+users.post('/new-user', function(req, res, next) {
+    var postData = req.body;  
+    var query =  'INSERT INTO users Set ?';
+	connection.query(query,postData, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": "New User Added"}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+/* GET users listing. */
+users.get('/all-users', function(req, res, next) {
+    var query = 'SELECT * from users';
+	connection.query(query, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+/* GET Specific User. */
+users.get('/:id', function(req, res, next) {
+    var query = 'SELECT * from users where id = ?';
+	connection.query(query, [req.params.id], function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+/* Edit User Info */
+users.post('/editUser/:id', function(req, res, next) {
+    var postData = req.body;    
+    var payload = [postData.username, postData.name, postData.password, postData.id];
+    var query = 'Update users SET username = ?, name=?, password=? where id=?';
+    connection.query(query, payload, function (error, results, fields) {                   ;
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": "User Details Updated"}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+/* Add New Vehicle Owner*/
+users.post('/new-owner', function(req, res, next) {
+	var postData = req.body;  
+	payload = [];
+    var query =  'INSERT INTO vehicle_owners Set ?';
+	connection.query(query,postData, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": "New Vehicle Owner Added!"}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+/* GET Vehicle Owners listing. */
+users.get('/owners', function(req, res, next) {
+    var query = 'SELECT * from vehicle_owners';
+	connection.query(query, function (error, results, fields) {
+	  	if(error){
+	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
+	  		//If there is error, we send the error in the error section with 500 status
+	  	} else {
+  			res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  			//If there is no error, all is good and response is 200OK.
+	  	}
+  	});
+});
+
+users.use(function(req, res, next) {
+    var token = req.body.token || req.headers['token'];
+    var appData = {};
+    if (token) {
+        jwt.verify(token, process.env.SECRET_KEY, function(err) {
+            if (err) {
+                appData["error"] = 1;
+                appData["data"] = "Token is invalid";
+                res.status(500).json(appData);
+            } else {
+                next();
+            }
+        });
+    } else {
+        appData["error"] = 1;
+        appData["data"] = "Please send a token";
+        res.status(403).json(appData);
+    }
+});
+
+module.exports = users;
