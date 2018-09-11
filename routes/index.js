decd4f09259827fdb25002cb60783b251146c8e3
Tayo Jabar <tayojabar@gmail.com>
2018-09-11 11:16:39 +0100

    ignored files folder

--- routes/index.js
+++ routes/index.js
@@ -98,7 +98,7 @@ router.get('/vehicles', function(req, res, next) {
 	  		res.send(JSON.stringify({"status": 500, "error": error, "response": null})); 
 	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
-  			res.send(JSON.stringify(results));
+  			res.send(results);
   			//If there is no error, all is good and response is 200OK.
 	  	}
   	});
