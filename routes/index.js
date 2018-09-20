0344f76aba3e77a50a6dfe44ad603b07b825e5dd
Tayo Jabar <tayojabar@gmail.com>
2018-09-20 15:28:22 +0100

    Backend

--- routes/index.js
+++ routes/index.js
@@ -862,7 +862,7 @@ router.post('/exterior-lights/:number_plate', function(req, res, next) {
 
 router.post('/body-frame/:number_plate', function(req, res, next) {
 	var postData = req.body; 
-	postData.Date_Inspected = moment().format('YYYY-MM-DD h:mm:ss a');
+	postData.Date_Inspected = moment().utcOffset('+0100').format('YYYY-MM-DD h:mm:ss a');
 	console.log(postData.Date_Inspected);
 	//console.log(moment());
 	postData.Vehicle = req.params.number_plate;
