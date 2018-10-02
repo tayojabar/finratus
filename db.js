ccafe72c97f52ba074f177b90baf69985b534230
ukaye <itaukemeabasi@gmail.com>
2018-10-02 13:35:34 +0100

    db updates

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 let mysql = require('mysql'),
-    config = require('config'),
+    config = require('./config'),
     status = process.env.STATUS || 'development',
     mysqlConfig = config[status],
     connection = mysql.createConnection(mysqlConfig);
