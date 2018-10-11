a96d1776c846cbd85452fae96624ad81ecd664c0
ukaye <itaukemeabasi@gmail.com>
2018-10-11 15:45:54 +0100

    inspection fix

--- routes/index.js
+++ routes/index.js
@@ -764,6 +764,7 @@ router.post('/mechanical-check/:id', function(req, res, next) {
     
     db.query(query, payload, function (error, results, fields) {
 	  	if(error){
+	  		console.log(error)
 	  		res.send({"status": 500, "error": error, "response": null}); 
 	  		//If there is error, we send the error in the error section with 500 status
 	  	} else {
