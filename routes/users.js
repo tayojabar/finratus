708eb4f422996331a04c3e33e9b335b9a9d47235
ukaye <itaukemeabasi@gmail.com>
2018-10-17 11:34:20 +0100

    atb cisco mail module

--- routes/users.js
+++ routes/users.js
@@ -506,12 +506,15 @@ users.post('/contact', function(req, res) {
 
 users.post('/sendmail', function(req, res) {
     let data = req.body;
+    if (data['solution[]'])
+        data.solution = data['solution[]'];
     if (!data.name || !data.email || !data.company || !data.phone || !data.solution || !data.description)
         return res.send("Error");
-    data.solution = data.solution.join(',');
+    if (data.solution.constructor === [].constructor)
+        data.solution = data.solution.join(',');
     data.date = moment().utcOffset('+0100').format('YYYY-MM-DD h:mm:ss a');
     let mailOptions = {
-        from: 'ATB Cisco',
+        from: 'ATB Cisco <applications@loan35.com>',
         to: 'itaukemeabasi@gmail.com',
         subject: 'ATB Cisco Application: '+data.name,
         template: 'mail',
