5119b19f85d9be4fe99f7ca576b020abca61ceed
ukaye <itaukemeabasi@gmail.com>
2018-12-06 13:40:30 +0100

    login password update

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 // Loads the environment variables from the .env file
 require('dotenv').config();
 
-var express = require('express');
-var mysql = require('mysql');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require('http');
-var path = require('path');
-const fileUpload = require('express-fileupload');
-const fs = require('fs');
-var session = require('client-sessions');
-var morgan = require('morgan');
-var db = require('./db');
-var cookie = require('cookie');
+let express = require('express');
+let fs = require('fs'),
+    db = require('./db'),
+    http = require('http'),
+    path = require('path'),
+    mysql = require('mysql'),
+    morgan = require('morgan'),
+    bcrypt = require('bcrypt'),
+    cookie = require('cookie'),
+    bodyParser = require('body-parser'),
+    session = require('client-sessions'),
+    cookieParser = require('cookie-parser'),
+    fileUpload = require('express-fileupload');
+
 //check and create uploads directory
 if (!fs.existsSync('./files')){
     fs.mkdirSync('./files');
@@ -30,23 +32,15 @@ if (fs.existsSync('./files')){
     }
 }
 
-var app = express();
-var index = require('./routes/index');
-var user = require('./routes/users');
-var cors = require('cors');
+let app = express(),
+    cors = require('cors'),
+    user = require('./routes/users'),
+    index = require('./routes/index');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
-
-//File Upload
+app.use(bodyParser.json());
 app.use(fileUpload());
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-// fix cors issues
 app.use(cors());
 
 //Session 
@@ -65,44 +59,29 @@ app.post('/login', function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
     db.query('SELECT *, (select role_name from user_roles r where r.id = user_role) as role FROM users WHERE username = ? and status = 1', username, function(err, rows, fields) {
-      if (err) {
-        //res.sendFile('/login', { error: 'Invalid email or password.' });
-        //res.redirect('/inspections');
-        // res.sendFile('index.html', { root: __dirname+'/views' });
-          res.send({"status": 500, "response": "Connection Error!"});
-      }
-      else if (rows.length === 0){
-          res.send({"status": 500, "response": "User Disabled!"});
-      }
-      else if (password === rows[0].password) {
-          // sets a cookie with the user's info
+      if (err)
+          return res.send({"status": 500, "response": "Connection Error!"});
 
+      if (rows.length === 0)
+          return res.send({"status": 500, "response": "User Disabled!"});
+
+      if (bcrypt.compareSync(password,rows[0].password)) {
           user = rows[0];
           db.query('SELECT id,module_id, (select module_name from modules m where m.id = module_id) as module_name, read_only, editable FROM permissions where role_id = ? and date in (select max(date) from permissions where role_id = ?) group by module_id', [user.user_role, user.user_role], function (error, perm, fields) {
               if (!error) {
-                  user.permissions = perm
-                  var query = 'select * from modules m where m.id in (select p.module_id from permissions p where read_only = 1 ' +
+                  user.permissions = perm;
+                  let query = 'select * from modules m where m.id in (select p.module_id from permissions p where read_only = 1 ' +
                                 'and p.role_id = ? and date in (select max(date) from permissions where role_id = ?) group by module_id) order by menu_name asc'
                   db.query(query, [user.user_role, user.user_role], function (er, mods, fields) {
                     user.modules = mods;
-                    // res.setHeader('Set-Cookie', cookie.serialize('user', rows[0], {
-                    //       httpOnly: true,
-                    //       maxAge: 60 * 60 * 24 * 7 // 1 week
-                    //   }));
                     res.send({"status": 200, "response": user});
                   });
-              }
-              else {
+              } else {
                   res.send({"status": 500, "response": "No permissions set for this user"})
               }
           });
-          //res.sendFile('dashboard.html', { root: __dirname+'/views' });
-      }
-      else {
-          //   res.sendFile('/login', { error: 'Invalid email or password.' });
-          //res.redirect('/login');
-          // res.sendFile('index.html', {root: __dirname + '/views'});
-          res.send({"status": 500, "response": "Login Unsuccessful!"});
+      } else {
+          res.send({"status": 500, "response": "Password is incorrect!"});
       }
     });
   });
@@ -113,13 +92,12 @@ app.use(function(req, res, next) {
         db.query('SELECT * FROM users WHERE email = ?', req.session.user.email, function(err, rows, fields) {
             if (!err) {
                 req.user = rows[0];
-                delete rows[0].password; // delete the password from the session
-                req.session.user = rows[0];  //refresh the session value
+                delete rows[0].password;
+                req.session.user = rows[0];
                 res.locals.user = rows[0];
             }
-            // finishing processing the middleware and run the route
             next();
-            });
+        });
     } else {
         next();
     }
@@ -136,16 +114,11 @@ function requireLogin (req, res, next) {
 app.get('/logout', function(req, res) {
     req.session.reset();
     res.redirect('/logon');
-    //res.sendFile('dashboard.html', { root: __dirname+'/views' });
   });
 
 app.use('/', index);
 app.use('/user', user);
 app.use('/files', express.static(__dirname + '/files'));
-
-// app.get('/login', function(req, res){
-//     res.sendFile('login.html', { root: __dirname+'/views' });
-// });
 
 app.get('/logon', function(req, res){
   res.sendFile('index.html', { root: __dirname+'/views' });
@@ -253,13 +226,13 @@ app.get('/client-info', requireLogin, function(req, res){
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
 module.exports = app;
-var server = http.createServer(app);
+let server = http.createServer(app);
 
 server.listen(process.env.port || process.env.PORT || 4000, function () {
     console.log('server running on %s [%s]', process.env.PORT, process.env.STATUS);
