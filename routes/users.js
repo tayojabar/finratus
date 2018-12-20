e86e8554547b25145dea2f833b0bceca5ba39b1b
ukaye <itaukemeabasi@gmail.com>
2018-12-20 15:36:04 +0100

    sendmail test

--- routes/users.js
+++ routes/users.js
@@ -2314,8 +2314,6 @@ users.post('/contact', function(req, res) {
 
 users.post('/sendmail', function(req, res) {
     let data = req.body;
-    if (data['solution[]'])
-        data.solution = data['solution[]'];
     if (!data.name || !data.email || !data.company || !data.phone || !data.title || !data.location || !data.description || !data.lead)
         return res.send("Required Parameters not sent!");
     data.date = moment().utcOffset('+0100').format('YYYY-MM-DD h:mm:ss a');
