69ca5a7a561078d3b3039edd984836e4868f6168
farquardsolve <fakuadesegun@live.com>
2019-02-19 17:06:35 +0100

    update@wired new investment product

---
+++ views/global-params.js
@@ -0,0 +1,49 @@
+var interest_conditions = [{
+        value: 'Monthly'
+    },
+    {
+        value: 'Up-Front'
+    }
+];
+
+var interest_calculation = [{
+        value: 'Daily'
+    },
+    {
+        value: 'Scheduler'
+    }
+];
+
+var schedules = [{
+        value: "Monthly"
+    },
+    {
+        value: 'Quaterly'
+    },
+    {
+        value: 'Half-Yearly'
+    },
+    {
+        value: 'Yearly'
+    }
+];
+
+var freq_withdrawal = [{
+        value: "Daily"
+    },
+    {
+        value: "Weekly"
+    },
+    {
+        value: "Monthly"
+    },
+    {
+        value: 'Quaterly'
+    },
+    {
+        value: 'Half-Yearly'
+    },
+    {
+        value: 'Yearly'
+    }
+];
