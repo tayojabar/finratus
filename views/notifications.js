01583d0f05b67536c7ba743917deea253000268c
Tayo Jabar <tayojabar@gmail.com>
2019-03-21 14:08:39 +0100

    Notification Update

--- views/notifications.js
+++ views/notifications.js
@@ -1,16 +1,26 @@
-let init = 0;
+let init = 0,
+    count = 0,
+    ids = [];
+
 function notifications(){
+    ids.length = 0;
+    count = 0;
     status = false;
     $.ajax({
         type: "GET",
-        url: "/notifications/new-updates",
+        url: "/notifications/new-updates?bug="+JSON.parse(localStorage.user_obj).ID,
         success: function (response) {
             status = true;
-            $('#noti-info').html('You have '+((init === 0)? 0 : (response.length - init))+ ' new notifications');
             init = response.length;
-            $('#noti-count').html(((init === 0)? 0 : (response.length - init)));
-            let count = 0,
-                icon,
+            if (response.length === 0){
+                $('#noti-count').hide();
+            }
+            else{
+                $('#noti-count').show();
+                $('#noti-count').html(response.length);
+            }
+            let icon,
+                link,
                 item;
             $('#n-dropdown').empty();
             $.each(response, function (key, val) {
@@ -18,12 +28,15 @@ function notifications(){
                 switch (val.category){
                     case 'Activity':
                         icon = '<i class="fa fa-tasks fa-4x"></i>'
+                        link = '/activity'
                         break;
                     case 'Clients':
                         icon = '<i class="fa fa-users fa-4x"></i>'
+                        link = '/client-info?id='+val.client;
                         break;
                     case 'Users':
                         icon = '<i class="fa fa-user fa-4x"></i>'
+                        link = '#';
                         break;
                 }
                 let buttonid = 'but'+val.notification_id;
@@ -31,42 +44,115 @@ function notifications(){
 '                            <p class="feed-body-header">'+$.timeago(val.date_created)+'</time></p>\n' +
 '                            <div class="row">\n' +
 '                                <span class="col-md-3" style="padding-right: 0">'+icon+'</span>\n' +
-'                                <span class="col-md-9" style="padding-left: 10px;font-size: 14px">'+val.description+'\n' +
+'                                <a href="'+link+'" class="col-md-9" style="padding-left: 10px;font-size: 14px">'+val.description+'\n' +
 '                                    <div class="client-notification">\n' +
 '                                        <img class="user-avatar rounded-circle" src="images/admin.jpg" alt="User Avatar"\n' +
 '                                             style="">\n' +
 '                                            <p>'+val.user+'</p>\n' +
-                    '                         <small class="feed-content-menu float-right" style="margin-top: 50px" id="'+buttonid+'">Mark as Viewed</small>\n'+
-// '                                        <div class="feed-content-menu float-right" id="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</div>\n' +
+                    '                         <small role="button" onclick="markAsViewed('+val.notification_id+')" class="feed-content-menu float-right mark" style="margin-top: 50px" id="'+buttonid+'">Mark as Viewed</small>\n'+
 '                                    </div>\n' +
-'                                 </span>\n' +
+'                                 </a>\n' +
 '                            </div>\n' +
-// '                            <div class="feed-content-menu-drp-dwn d-none" id="feed-content-menu-drp-dwn"\n' +
-// '                                 aria-labelledby="dropdownMenuButton">\n' +
-// '                                <a class="dropdown-item" href="#">Mark as Viewed</a>\n' +
-// '                                <a class="dropdown-item" href="#">Something else here</a>\n' +
-// '                            </div>\n' +
 '                        </div>'
                 $('#n-dropdown').append(item);
-                let obj = {};
-                obj.notification_id = val.notification_id;
-                   $.ajax({
-                       type: "GET",
-                       url: "/notifications/update-pr",
-                       data:obj,
-                       success: function (response) {
-                           // console.log('Success');
-                       }
-                   });
+                // let obj = {};
+                // obj.notification_id = val.notification_id;
+                // obj.val = 2;
+                //    $.ajax({
+                //        type: "GET",
+                //        url: "/notifications/update-pr",
+                //        data:obj,
+                //        success: function (response) {
+                //            // console.log('Success');
+                //            count = 0;
+                //        }
+                //    });
             });
-
+            $('#noti-info').html(count+ ' unread notifications!');
         }
     });
 }
 setInterval(notifications, 5000);
 
+let cats;
+function list_categories(){
+    $.ajax({
+        type: "GET",
+        url: "/notifications/categories",
+        success: function (response) {
+            cats = response;
+            let count = response.length,
+                item;
+            $('#n-settings-panel').empty();
+            $('#n-settings-panel').append('<h6>Manage Notifications</h6><br/>');
+            for (let i = 0; i < count; i++){
+                let v = response[i];
+                if (v.compulsory === '1'){
+                    item = '<input type="checkbox" id="act'+v.id+'" disabled="disabled"/>&nbsp;&nbsp;<label for = "'+v.category_name+'">'+v.category_name+'</label><hr/>'
+                }
+                else{
+                    item = '<input type="checkbox" id="act'+v.id+'"/>&nbsp;&nbsp;<label for = "'+v.category_name+'">'+v.category_name+'</label><hr/>'
+                }
+                $('#n-settings-panel').append(item);
+            }
+            let button = '<button id="submit-settings" onclick="savePreferences()" class="btn btn-info fa-pull-right">Save <i class="fa fa-save"></i></button><br/>';
+            $('#n-settings-panel').append(button);
+        }
+    });
+}
+
 function manage(){
-    // alert('Here');
     $('#n-settings-panel').slideDown('slow');
+    list_categories();
     $('#n-dropdown').hide();
+}
+
+function savePreferences(){
+    var obj = {};
+    var arr = [];
+    var i = 0; var j = 0;
+    for (let a = 0; a < cats.length; a++){
+        let rd; let wt;
+        rt = ($('#act'+cats[a]["id"]).prop('checked')) ? 1 : 0;
+        arr[a]=[cats[a]["id"], rt];
+    }
+
+    obj.userid = JSON.parse(localStorage.user_obj).ID;
+    obj.cats = arr;
+    var test = [];
+    $.ajax({
+        'url': 'notifications/savePreferences/'+JSON.parse(localStorage.user_obj).ID,
+        'type': 'post',
+        'data': obj,
+        'success': function (data) {
+            $.each(data, function (key, val) {
+                test[key] = val;
+            });
+//                console.log(test);
+            if(test.status == 500){
+                swal("Failed!", "Error encountered. Please try again.", "error");
+            }
+            else
+                swal("Success!", "Notification Preferences Set!", "success");
+            $('#n-dropdown').slideUp('slow');
+            $('#n-settings-panel').hide();
+        }
+    });
+}
+
+$( ".mark" ).click(function() {
+    alert( "Handler for .click() called." );
+});
+
+function markAsViewed(id){
+    let obj = {};
+    obj.notification_id = id;
+    obj.val = 3;
+    $.ajax({
+        type: "GET",
+        url: "/notifications/update-pr",
+        data:obj,
+        success: function (response) {
+        }
+    });
 }
