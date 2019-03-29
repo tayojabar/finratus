b01c21362901afde8c256f282f93eb71fe143e49
ukaye <itaukemeabasi@gmail.com>
2019-03-29 17:18:22 +0100

    service debugging

--- routes/service/core-service.js
+++ routes/service/core-service.js
@@ -4,6 +4,7 @@ const db = require('../../db');
 const router = express.Router();
 
 router.get('/get', function (req, res, next) {
+    console.log(req)
     let query = req.query.query;
     db.query(query, function (error, results, fields) {
         console.log(results)
