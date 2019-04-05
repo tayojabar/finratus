693f77413520246188eedfba25638fd74543a94b
ukaye <itaukemeabasi@gmail.com>
2019-04-05 11:21:52 +0100

    debugging

--- routes/service/custom-services/client.service.js
+++ routes/service/custom-services/client.service.js
@@ -54,13 +54,13 @@ router.post('/mandate/setup', function (req, res, next) {
         mandateType: 'DD',
         maxNoOfDebits: '100'
     }, function (payload, setup_response) {
-        console.log(setup_response)
         if (setup_response && setup_response.mandateId) {
             let authorize_payload = {
                 mandateId: setup_response.mandateId,
                 requestId: setup_response.requestId
             };
             helperFunctions.authorizeMandate(authorize_payload, function (authorization_response) {
+                console.log(authorization_response)
                 if (authorization_response && authorization_response.remitaTransRef){
                     payload.remitaTransRef = authorization_response.remitaTransRef;
                     payload.mandateId = setup_response.mandateId;
