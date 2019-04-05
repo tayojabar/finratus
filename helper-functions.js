beb0e9d5477af45b4e91977403e05ac4794a3cc5
ukaye <itaukemeabasi@gmail.com>
2019-04-05 11:53:25 +0100

    debugging

--- helper-functions.js
+++ helper-functions.js
@@ -139,7 +139,6 @@ functions.authorizeMandate = function (payload, callback) {
     headers.MERCHANT_ID = process.env.REMITA_MERCHANT_ID;
     headers.API_DETAILS_HASH = SHA512(headers.API_KEY + headers.REQUEST_ID + process.env.REMITA_API_TOKEN);
     headers.REQUEST_TS = functions.remitaTimeStampFormat(date);
-    console.log(`${process.env.REMITA_BASE_URL}/requestAuthorization`)
     request.post(
         {
             url: `${process.env.REMITA_BASE_URL}/requestAuthorization`,
@@ -148,8 +147,11 @@ functions.authorizeMandate = function (payload, callback) {
             json: true
         },
         (error, res, body) => {
+            console.log('Error=================')
             console.log(error)
+            console.log('Body==================')
             console.log(body)
+            console.log('Res===================')
             console.log(res)
             if (error) {
                 return callback(error);
